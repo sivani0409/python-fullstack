a30250a27e345d8e96e2ae3fa8ceb5716e110d77
sivani0409 <sivanigokavarapu@gmail.com>
2025-06-09 15:52:40 +0530

    bhavya

---
+++ day12/basic.js
@@ -0,0 +1,56 @@
+// let x=20
+// console.log(x)
+// let x=30
+// console.log(x)
+// const y="sivani"
+// console.log(y)
+// const y="20"
+// // console.log(y)
+// let block
+// {
+//     let x=30
+//     console.log(x)
+
+// }
+// console.log(x)
+
+// operators and comaritions
+// let a=4
+// let b=30
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a%b)
+// console.log(a/b)
+// console.log(a<b)
+// console.log(a<=b)
+// console.log(a==b)
+// console.log(a>=b)
+
+// for loop
+// if(a<b)
+//     console.log("a is smaller")
+// else if(a>b)
+//     console.log("b is smaller")
+// else
+//     console.log("both are equal")
+// for(let i = 30;i > 0;i-- )
+//     console.log(i)
+// for(let i =30;i > 0;i-=2)
+// console.log(i)
+
+
+// while loop
+let x=12
+let y=21
+
+do
+{
+    console.log(y)
+    y--;
+}
+while(x>y)
+{
+    console.log(x)
+    
+}
